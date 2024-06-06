49d30750b33d279bc83748a80a48d1e9ba1f17c9
ESDLIFE\serenali <serenali@esdlife.com>
2024-06-06 15:43:44 +0800

    add mysql config

--- src/main/java/online/shopping/system/customer/service/dao/Customer.java
+++ src/main/java/online/shopping/system/customer/service/dao/Customer.java
@@ -6,13 +6,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(schema = "customer")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
 
     @Id
@@ -34,4 +45,14 @@ public class Customer {
 
     @Column(name = "phone")
     private int phone;
+
+    @Column(name = "created_on")
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdOn;
+
+    @Column(name = "last_modified_on")
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastModifiedOn;
 }
