a91aefa2055153777ff876b31b29ee4ff23c9898
ESDLIFE\serenali <serenali@esdlife.com>
2024-07-23 11:59:35 +0800

    add keyclock

---
+++ src/main/java/online/shopping/system/customer_service/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package online.shopping.system.customer_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity){
+        return httpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
