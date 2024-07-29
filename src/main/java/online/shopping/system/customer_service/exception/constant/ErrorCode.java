b9f77671cd5e9538855d055638c93992b3206067
ESDLIFE\serenali <serenali@esdlife.com>
2024-07-29 18:26:01 +0800

    add exception handler

---
+++ src/main/java/online/shopping/system/customer_service/exception/constant/ErrorCode.java
@@ -0,0 +1,31 @@
+package online.shopping.system.customer_service.exception.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import online.shopping.system.customer_service.exception.ExceptionCode;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements ExceptionCode {
+    CUSTOMER_ALREADY_EXIST("C0001","customer with username: {} is already existed"),
+
+    SYSTEM_BUSY("C0002","System busy");
+
+
+    private final String errorCode;
+    private final String message;
+
+    @Override
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage(Object... arguments) {
+        if(arguments == null){
+            return message;
+        }
+        return new ParameterizedMessage(message, arguments).getFormattedMessage();
+    }
+}
